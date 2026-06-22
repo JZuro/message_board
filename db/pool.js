098c0a1e589f21245a6998aff4056c19ea3b760d
rygyfygy <237001230+rygyfygy@users.noreply.github.com>
2026-06-22 20:37:34 +0200

    Set up DATABASE_URL for railway intead of separate env variables

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,5 @@
 const { Pool } = require("pg");
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
 module.exports = new Pool({
-	host: process.env.PG_HOST,
-	user: process.env.PG_USER,
-	database: process.env.PG_NAME,
-	password: process.env.PG_PASSWORD,
-	port: Number(process.env.PG_PORT),
+	connectionString: process.env.DATABASE_URL,
 });
